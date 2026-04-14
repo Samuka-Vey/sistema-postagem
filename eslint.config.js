6c48fe1b893a7566dd6c954014bdafdd4c8ee789
samuka-vey <marcosbarros@aluno.uespi.br>
2026-04-14 11:15:42 -0300

    chore: update ESLint rules and add initial index file

--- eslint.config.js
+++ eslint.config.js
@@ -15,9 +15,11 @@ export default [
     },
     rules: {
       'prettier/prettier': 'error',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn', // Adiciona esta regra para evitar variáveis não utilizadas
+      '@typescript-eslint/no-explicit-any': 'off', // Desativa esta regra para permitir o uso de 'any' quando necessário
+      '@typescript-eslint/explicit-function-return-type': 'error', // Adiciona esta regra para garantir que as funções tenham tipos de retorno explícitos
+      '@typescript-eslint/explicit-module-boundary-types': 'error', // Adiciona esta regra para garantir que as funções exportadas tenham tipos de retorno explícitos
+      '@typescript-eslint/no-inferrable-types': 'error', // Adiciona esta regra para evitar tipos inferíveis
     },
   },
 ];
